00bb9305640c39aa132faee5d7a7f8721c70ce8f
sriraj21 <srirajr9@gmail.com>
2025-09-10 18:18:30 +0530

    Add files via upload

---
+++ firebase-config.js
@@ -0,0 +1,8 @@
+        const firebaseConfig = {
+          apiKey: "AIzaSyDHVU7fE_Mez80jLxezXpa6wt_7RMBZiKE",
+          authDomain: "family-tracker-app-7d5f9.firebaseapp.com",
+          projectId: "family-tracker-app-7d5f9",
+          storageBucket: "family-tracker-app-7d5f9.firebasestorage.app",
+          messagingSenderId: "543941177603",
+          appId: "1:543941177603:web:b33beb54ab9b97098bf936"
+        };
